0067d078bd59557ee3f394311f5a2bc672d69b95
mlu-12 <59448950+mlu-12@users.noreply.github.com>
2020-01-06 13:42:08 -0500

    Update Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -1,88 +1,124 @@
 public class Lab4
 {
-  public static void turnRight()
-  {
-    Robot.turnLeft();
-    Robot.turnLeft();
-    Robot.turnLeft();
-  }
-  
-  public static void turnAround()
-  {
-    Robot.turnLeft();
-    Robot.turnLeft();
-  }
+    public static void turnRight()
+    {
+        Robot.turnLeft();
+        Robot.turnLeft();
+        Robot.turnLeft();
+    }
 
-  public static void backUp()
-  {
-    turnAround();
-    Robot.move();
-    turnAround();
-  }
-  
-  public static void completeBars()
-  {
-    //insert instructions below
+    public static void turnAround()
+    {
+        Robot.turnLeft();
+        Robot.turnLeft();
+    }
+
+    public static void backUp()
+    {
+        turnAround();
+        Robot.move();
+        turnAround();
+    }
+
+    public static void completeBars()
+    {
+        while(Robot.frontIsClear())
+        {
+            completeOneBar();
+        }
+        completeLastBar();
+    }
+
+    public static void completeLastBar()
+    //precondition: Robot is facing east in the last column
+    //postcondition: Robot has darkened the last column
+    {
+        Robot.turnLeft();
+        while(!Robot.onDark())
+        {
+            Robot.makeDark();
+            Robot.move();
+        }
+        turnAround();
+        while(Robot.frontIsClear())
+        {
+            Robot.move();
+        }
+        Robot.turnLeft();
+    }
+
+    public static void completeOneBar()
+    //precondition: Robot is facing east at the bottom of a column
+    //postcondition: Robot has darkened the entire column, is facing east at the bottom of the next column
+    {
+        Robot.turnLeft();
+        while(!Robot.onDark())
+        {
+            Robot.makeDark();
+            Robot.move();
+        }
+        turnAround();
+        while(Robot.frontIsClear())
+        {
+            Robot.move();
+        }
+        Robot.turnLeft();
+        Robot.move();
+    }
+
+    public static void testCompleteBars1()
+    {
+        Robot.load("bars1.txt");
+        Robot.setDelay(0.025);
+        completeBars();
+    }
+
+    public static void testCompleteBars2()
+    {
+        Robot.load("bars2.txt");
+        Robot.setDelay(0.025);
+        completeBars();
+    }
+
+    public static void combinePiles()
+    {
+        //insert instructions below
 
     
-    
-  }
-  
-  public static void testCompleteBars1()
-  {
-    Robot.load("bars1.txt");
-    Robot.setDelay(0.025);
-    completeBars();
-  }
-  
-  public static void testCompleteBars2()
-  {
-    Robot.load("bars2.txt");
-    Robot.setDelay(0.025);
-    completeBars();
-  }
-  
-  public static void combinePiles()
-  {
-    //insert instructions below
+    }
+
+    public static void testCombinePiles1()
+    {
+        Robot.load("piles1.txt");
+        Robot.setDelay(0.025);
+        combinePiles();
+    }
+
+    public static void testCombinePiles2()
+    {
+        Robot.load("piles2.txt");
+        Robot.setDelay(0.025);
+        combinePiles();
+    }
+
+    public static void connectDots()
+    {
+        //insert instructions below
 
     
-    
-  }
+    }
 
-  public static void testCombinePiles1()
-  {
-    Robot.load("piles1.txt");
-    Robot.setDelay(0.025);
-    combinePiles();
-  }
-  
-  public static void testCombinePiles2()
-  {
-    Robot.load("piles2.txt");
-    Robot.setDelay(0.025);
-    combinePiles();
-  }
-  
-  public static void connectDots()
-  {
-    //insert instructions below
+    public static void testConnectDots1()
+    {
+        Robot.load("connect1.txt");
+        Robot.setDelay(0.025);
+        connectDots();
+    }
 
-    
-    
-  }
-  
-  public static void testConnectDots1()
-  {
-    Robot.load("connect1.txt");
-    Robot.setDelay(0.025);
-    connectDots();
-  }
-  
-  public static void testConnectDots2()
-  {
-    Robot.load("connect2.txt");
-    Robot.setDelay(0.025);
-    connectDots();
-  }
+    public static void testConnectDots2()
+    {
+        Robot.load("connect2.txt");
+        Robot.setDelay(0.025);
+        connectDots();
+    }
 }
